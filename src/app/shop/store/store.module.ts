0d29d4aa52604f4dbff86492b53175a5dcf214bd
rotarrares <rotarrares95@gmail.com>
2019-08-28 23:26:18 +0300

    1 file modified

--- src/app/shop/store/store.module.ts
+++ src/app/shop/store/store.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {StoreComponent} from './store.component';
+
 
  const routes: Routes = [{
     path: '/home',
